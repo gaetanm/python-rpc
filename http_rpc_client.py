c069132afcf2841bbdb0aab6b9498be8cd4adb35
Gaëtan Masson <16129111@bjtu.edu.cn>
2016-09-19 16:09:41 +0800

    Call existing serv resources in the client

---
+++ http_rpc_client.py
@@ -0,0 +1,20 @@
+import requests
+
+
+class Client:
+    def __init__(self, host):
+        self.host = host
+
+    def get(self, resource):
+        return requests.get(self.host + "/" + resource).json()
+
+    def time(self):
+        return self.get("time")['time']
+
+    def ram(self):
+        r = self.get("ram")
+        return r['total'], r['used']
+
+    def hdd(self):
+        r = self.get("hdd")
+        return r['total'], r['used']
