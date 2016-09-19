a25c0dc8f28c71db083ba3009cea9e959006e46b
Gaëtan Masson <16129111@bjtu.edu.cn>
2016-09-19 16:09:28 +0800

    Add current serv methods

--- http_rpc_server.py
+++ http_rpc_server.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify
+import time
+import psutil
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/time')
+def current_timestamp():
+    return jsonify(time=int(time.time()))
+
+
+@app.route('/ram')
+def ram_status():
+    return jsonify(total=psutil.virtual_memory()[0],
+                   used=psutil.virtual_memory()[3])
+
+
+@app.route('/hdd')
+def hdd_status():
+    return jsonify(total=psutil.disk_usage("/")[0],
+                   used=psutil.disk_usage("/")[1])
 
 
 if __name__ == '__main__':
