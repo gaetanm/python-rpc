c6c66d3e232503d6687744c8c4b1bd1953d7e4c1
Gaëtan Masson <16129111@bjtu.edu.cn>
2016-09-21 17:30:22 +0800

    Update Exception logic

--- http_rpc_client.py
+++ http_rpc_client.py
@@ -8,7 +8,7 @@ class Client:
     def get(self, resource, payload=None):
         try:
             return requests.get(self.host + "/" + resource, params=payload).json()
-        except requests.exceptions.ConnectionError:
+        except:
             raise Exception("server unreachable")
 
     def time(self):
@@ -23,22 +23,16 @@ class Client:
         return r['total'], r['used']
 
     def add(self, a, b):
-        try:
-            a += 1
-            b += 1
-            r = self.get("add", {'a': a, 'b': b})
-            return r['result']
-        except TypeError:
+        if isinstance(a, int) is False or isinstance(b, int) is False:
             raise Exception("not an integer")
+        r = self.get("add", {'a': a, 'b': b})
+        return r['result']
 
     def sub(self, a, b):
-        try:
-            a += 1
-            b += 1
-            r = self.get("sub", {'a': a, 'b': b})
-            return r['result']
-        except TypeError:
+        if isinstance(a, int) is False or isinstance(b, int) is False:
             raise Exception("not an integer")
+        r = self.get("sub", {'a': a, 'b': b})
+        return r['result']
 
     def json_to_xml(self, json_str):
         r = self.get("json_to_xml", {'json_str': json_str})
